fbbff634267c312b3c4f8a05f9a875ae2c0120ea
dmitriylosev8@gmail.com <dmitriylosev8@gmail.com>
2022-07-07 16:59:15 +0300

    объединение в одну коллекцию:  Доделать

--- Nika/Program.cs
+++ Nika/Program.cs
@@ -9,13 +9,15 @@ namespace CSLight
     internal class Program
     {
         static void Main(string[] args)
-        {      //Задание: объединение в одну коллекцию:    
+        {      //Задание: объединение в одну коллекцию:  Доделать  
 
             string[] chairs = { "1", "2", "3", "7", "10", "45" };
             string[] armchairs = { "4", "3", "5", "10", "18" };
             List<string> seats = new List<string>();
             seats.AddRange(chairs);
             seats.AddRange(armchairs);
+
+            
 
             SearchAndDeleteRepeatingStrings(seats, chairs, armchairs);
             ShowingAllUniqueSeats(seats);
@@ -25,17 +27,65 @@ namespace CSLight
         {
             string repeatingString;
 
-            for (int i = 0; i < chairs.Length; i++)
+            List<string> tempSeats = seats;
+
+
+            int count =0;
+
+           
+            for (int i = 0; i < seats.Count; i++)
             {
-                for (int j = 0; j < armchairs.Length; j++)
+                for (int j =0; j < tempSeats.Count; j++)
                 {
-                    if (chairs[i] == armchairs[j])
+                    if (seats[i] == tempSeats[j])
                     {
-                        repeatingString = armchairs[j];
-                        seats.Remove(repeatingString);
+                        repeatingString = seats[i];
+                        count++;
+                        if (count > 1)
+                        {
+                            //repeatingString = seats[i];
+                           // Console.WriteLine("хай");
+
+                            seats.Remove(repeatingString);
+                        }
                     }
                 }
-            }
+            }   
+            
+            
+            
+            //for (int i = 0; i < seats.Count; i++)
+           // {
+           //    for (int j = 0; j < seats.Count; j++)
+           //     {
+
+           //         if (seats[i] == seats[j])
+           //         {
+           //             repeatingString = seats[i];
+           //             count++;
+           //             if (count > 1)
+           //             {
+           //                 //repeatingString = seats[i];
+           //                 Console.WriteLine("хай");
+
+           //                 // seats.Remove(repeatingString);
+           //             }
+           //         }
+                    
+           //     }
+           // }
+
+            //for (int i = 0; i < chairs.Length; i++)
+            //{
+            //    for (int j = 0; j < armchairs.Length; j++)
+            //    {
+            //        if (chairs[i] == armchairs[j])
+            //        {
+            //            repeatingString = armchairs[j]
+            //            seats.Remove(repeatingString);
+            //        }
+            //    }
+            //}
         }
 
         static void ShowingAllUniqueSeats(List<string> seats)
