8303e06fcbbbbb691d3b6ed44506b00503410703
dmitriylosev8@gmail.com <dmitriylosev8@gmail.com>
2022-08-19 16:27:04 +0300

    Топ игроков

--- Nika/Program.cs
+++ Nika/Program.cs
@@ -9,66 +9,47 @@ namespace CSLight
     internal class Program
     {
         static void Main(string[] args)
-        {      // Задание: Поиск преступника:
+        {      // Задание: Топ игроков:
 
-            List<Сriminal> criminals = new List<Сriminal> {new Сriminal(170,65,false,"Русский", "Жора"), new Сriminal(178, 80, false, "Русский", "Гена"), new Сriminal(165, 58, true, "Армянин", "Ашот"), 
-                new Сriminal(181, 95, false, "Грузин", "Гиви"),new Сriminal(176,60,false,"Армянин", "Артур"),new Сriminal(160,48,false,"Грек", "Даниил"),new Сriminal(192,102,false,"Русский", "Коля"),
-                new Сriminal(173,86,true,"Русский", "Саша")};
+            List<Player> players = new List<Player> { new Player("Бритва", 85, 40), new Player("Дымок", 70, 43), new Player("Терминатор", 15, 10), new Player("Гроза", 105, 54), new Player("Пуля", 67, 58),
+            new Player("Девятка", 90, 87),new Player("Гараж", 74, 60),new Player("Буря", 10, 2),new Player("Страх", 150, 180),new Player("Нож", 11, 18),};
 
-            int weightOfCriminal;
-            int growthOfCriminal;
-            Console.WriteLine("Введите рост преступника ДО какого показателя вам необходимо:");
-            string userInput = Console.ReadLine();
-            bool isSuccessful = int.TryParse(userInput, out growthOfCriminal);
-            
-            if (isSuccessful)
+            var firstTopLevelPlayer = players.Max(player => player.Level);
+            var secondTopLevelPlayer = players.Where(player => player.Level < firstTopLevelPlayer).Max(player => player.Level);
+            var thirdTopLevelPlayer = players.Where(player => player.Level < secondTopLevelPlayer).Max(player => player.Level);
+            var topThreeLevelPlayers = players.Where(player => player.Level >= thirdTopLevelPlayer);
+
+            Console.WriteLine("Вот топ 3 игроков по уровню:\n");
+           
+            foreach(var player in topThreeLevelPlayers)
             {
-                Console.WriteLine("Введите вес преступника ДО какого показателя вам необходимо:");
-                userInput = Console.ReadLine();
-                isSuccessful = int.TryParse(userInput, out weightOfCriminal);
-
-                if (isSuccessful)
-                {
-                    Console.WriteLine("Введите национальность преступника:");
-                    string nationalityOfCriminal = Console.ReadLine();
-
-                    var criminalsWithNecessaryGrowth = criminals.Where(criminal => criminal.Growth < growthOfCriminal);
-                    var criminalsWithNecessaryGrowthAndWeight = criminalsWithNecessaryGrowth.Where(criminal => criminal.Weight < weightOfCriminal);
-                    var criminalsWithNecessaryGrowthAndWeightAndNationality = criminalsWithNecessaryGrowthAndWeight.Where(criminal => criminal.Nationality == nationalityOfCriminal);
-                    var necessaryCriminals = criminalsWithNecessaryGrowthAndWeightAndNationality.Where(criminal => criminal.IsArrested == false);
-
-                    foreach (var necessaryCriminal in necessaryCriminals)
-                    {
-                        Console.WriteLine(necessaryCriminal.Name);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Вы ввели не число.");
-                }
+                Console.WriteLine(player.Name);
             }
-            else
+
+            var firstTopPowerPlayer = players.Max(player => player.Power);
+            var secondTopPowerPlayer = players.Where(player => player.Power < firstTopPowerPlayer).Max(player => player.Power);
+            var thirdTopPowerPlayer = players.Where(player => player.Power < secondTopPowerPlayer).Max(player => player.Power);
+            var topThreePowerPlayers = players.Where(player => player.Power >= thirdTopPowerPlayer);
+
+            Console.WriteLine("\n\nВот топ 3 игроков по силе:\n");
+
+            foreach (var player in topThreePowerPlayers)
             {
-                Console.WriteLine("Вы ввели не число.");
+                Console.WriteLine(player.Name);
             }
-        }             
+        }     
     }
 
-    class Сriminal
+    class Player
     {
-        public string Name { get; private set; }    
-        public int Growth { get; private set; }
-        public int Weight { get; private set; }
-        public bool IsArrested { get; private set; }
-        public string Nationality { get; private set; }
-       
-        public Сriminal(int growth, int weight, bool isArrested, string nationality, string name)
+       public string Name { get; private set; }
+       public int Level { get; private set; }
+       public int Power { get; private set; }
+        public Player(string name, int level, int power)
         {
-            Growth = growth;
-            Weight = weight;
-            IsArrested = isArrested;
-            Nationality = nationality;
             Name = name;
+            Level = level;
+            Power = power;
         }
     }
 }
