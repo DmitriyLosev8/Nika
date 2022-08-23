bb1e2069556b7f6949683f126b39b8d69aa8ff46
dmitriylosev8@gmail.com <dmitriylosev8@gmail.com>
2022-08-23 10:57:19 +0300

    Объединение войск

--- Nika/Program.cs
+++ Nika/Program.cs
@@ -17,34 +17,24 @@ namespace CSLight
             List<Warrior> rigthWarriors = new List<Warrior> { (new Warrior("Саша", "Задорожный")), (new Warrior("Дима", "Лосев")), (new Warrior("Сергей", "Бодров")),
                 (new Warrior("Сергей", "Шнуров")),(new Warrior("Незами", "Мамедов")),(new Warrior("Ксения", "Задорожная")),(new Warrior("Любовь", "Двойнина")),(new Warrior("Владимир", "Двойнин")), };
 
-           var unitedSqaud = rigthWarriors.Union(leftWarriors.Where(warrior => warrior.LastName.StartsWith("Б"))).ToList();
-           rigthWarriors = unitedSqaud;
-          
-            for (int i = 0; i < leftWarriors.Count; i++)
-            {
-                for (int j = 0; j < rigthWarriors.Count; j++)
-                {
-                    if (leftWarriors[i] == rigthWarriors[j])
-                    {
-                        leftWarriors.RemoveAt(i);
-                    }
-                }
-            }
-
+            var unitedSqaud = rigthWarriors.Union(leftWarriors.Where(warrior => warrior.LastName.StartsWith("Б"))).ToList();
+            rigthWarriors = unitedSqaud;
+            var warriorsWithAFirst = leftWarriors.OrderBy(warrior => warrior.LastName).TakeWhile(warrior => warrior.LastName.StartsWith("А")).ToList();
+            var warriorsWithOutAAndBFirst = leftWarriors.OrderBy(warrior => warrior.LastName).SkipWhile(warrior => warrior.LastName.StartsWith("А")).SkipWhile(warrior => warrior.LastName.StartsWith("Б"));
+            var newLeftWarriors = warriorsWithOutAAndBFirst.Union(warriorsWithAFirst.Where(warrior => warrior.LastName.StartsWith("А"))).ToList();
             Console.WriteLine("Левый отряд без бойцов с фамилией на букву Б:");
-            
-            foreach (var warrior in leftWarriors)
+
+            foreach (var warrior in newLeftWarriors)
             {
                 Console.WriteLine(warrior.Name + " " + warrior.LastName);
             }
 
             Console.WriteLine("\n\nПравый отряд с перенесёнными бойцами с фамилией на букву Б:");
-           
+
             foreach (var warrior in rigthWarriors)
             {
                 Console.WriteLine(warrior.Name + " " + warrior.LastName);
             }
-
         }
     }
 
