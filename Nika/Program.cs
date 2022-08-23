2989c91c5fb9c76d9bf80c85719d826d01dfe07b
dmitriylosev8@gmail.com <dmitriylosev8@gmail.com>
2022-08-23 15:05:15 +0300

    Объединение войск

--- Nika/Program.cs
+++ Nika/Program.cs
@@ -9,84 +9,45 @@ namespace CSLight
     internal class Program
     {
         static void Main(string[] args)
-        {      // Задание: Анархия в больнице:
+        {      // Задание: Объединение войск:
 
-            List<Patient> patients = new List<Patient> {new Patient("Анжуров Никита Семёнович", 24, "Ангина"), new Patient("Жмышенко Валерий Альбертович", 54, "Стафилокок"), new Patient("Борисенко Дмитрий Степанович", 29, "Грипп"),
-          new Patient("Степанян Кристина Артуровна", 35, "ОРВИ"),new Patient("Данилова Анастасия Анатольевна", 19, "Астма"),new Patient("Жуков Степан Андреевич", 69, "Остеохондроз"),new Patient("Носов Николай Артёмович", 14, "Грипп"),
-          new Patient("Губарев Андрей Вячеславович", 42, "Перелом ноги"),new Patient("Мурзук Юлия Александровна", 62, "Сердечная недостаточность"),new Patient("Андропов Артём Витальевич", 35, "Шизофрения"),};
+            List<Warrior> leftWarriors = new List<Warrior> { (new Warrior("Гена", "Борисов")), (new Warrior("Дима", "Аникеев")), (new Warrior("Саша", "Бондаренко")),
+                (new Warrior("Саша", "Степанов")),(new Warrior("Боря", "Андропов")),(new Warrior("Семён", "Бонько")),(new Warrior("Никита", "Опа")),(new Warrior("Степан", "Борислав")), };
 
-            bool isWorking = true;
-            string sortFullNames = "1";
-            string sortAges = "2";
-            string showSomeDiseases = "3";
+            List<Warrior> rigthWarriors = new List<Warrior> { (new Warrior("Саша", "Задорожный")), (new Warrior("Дима", "Лосев")), (new Warrior("Сергей", "Бодров")),
+                (new Warrior("Сергей", "Шнуров")),(new Warrior("Незами", "Мамедов")),(new Warrior("Ксения", "Задорожная")),(new Warrior("Любовь", "Двойнина")),(new Warrior("Владимир", "Двойнин")), };
 
-            while (isWorking)
+            var newRigthWarriors = rigthWarriors.Union(leftWarriors.Where(warrior => warrior.LastName.StartsWith("Б"))).ToList();
+            var newLeftWarriors = leftWarriors.RemoveAll(warrior => warrior.LastName.StartsWith("Б"));
+            
+                
+            leftWarriors.RemoveAll(warrior => warrior.LastName.StartsWith("Б"));
+            rigthWarriors = newRigthWarriors;
+            Console.WriteLine("Левый отряд без бойцов с фамилией на букву Б:\n");
+
+            foreach (var warrior in leftWarriors)
             {
-                Console.SetCursorPosition(45, 0);
-                Console.WriteLine("Перед вами меню управления списком пациентов:");
-                Console.WriteLine($"Нажмите {sortFullNames}, чтобы отсортировать пациентов по ФИО\nНажмите {sortAges}, чтобы отсортировать пациентов по возрасту\nНажмите {showSomeDiseases}, чтобы найти пациентов по болезни\n" +
-                    $"Нажмите любую другую клавишу, чтобы выйти");
-                string userInput = Console.ReadLine();
-
-                if (userInput == "1")
-                {
-                    var sortByFullNamesPatients = patients.OrderBy(patient => patient.FullName).ToList();
-                    ShowPatiens(sortByFullNamesPatients);
-                }
-                else if (userInput == "2")
-                {
-                    var sortByAgePatient = patients.OrderBy(patient => patient.Age).ToList();
-                    ShowPatiens(sortByAgePatient);
-                }
-                else if (userInput == "3")
-                {
-                    var necessaryDiseases = FindNesecessaryDisease(patients);
-                    ShowPatiens(necessaryDiseases);
-                }
-                else
-                {
-                    isWorking = false;
-                }
-
-                Console.ReadKey();
-                Console.Clear();
+                Console.WriteLine(warrior.Name + " " + warrior.LastName);
             }
 
-            static void ShowPatiens(List<Patient> patients)
+            Console.WriteLine("\n\nПравый отряд с перенесёнными бойцами с фамилией на букву Б:\n");
+
+            foreach (var warrior in rigthWarriors)
             {
-                foreach (var patient in patients)
-                {
-                    Console.WriteLine($"{patient.FullName} - {patient.Age} - {patient.Disease}");
-                }
-            }
-
-            static List<Patient> FindNesecessaryDisease(List<Patient> patients)
-            {
-                Console.WriteLine("Введите болезнь, которую ищите:");
-                string userInput = Console.ReadLine();
-                var necessaryDiseases = patients.Where(patient => patient.Disease == userInput).ToList();
-
-                if (necessaryDiseases.Count == 0)
-                {
-                    Console.WriteLine("Пациента с такой болезнью нет в базе данных");
-                }
-
-                return necessaryDiseases;
+                Console.WriteLine(warrior.Name + " " + warrior.LastName);
             }
         }
     }
 
-    class Patient
+    class Warrior
     {
-        public string FullName { get; private set; }
-        public int Age { get; private set; }
-        public string Disease { get; private set; }
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
 
-        public Patient(string fullName, int age, string disease)
+        public Warrior(string name, string lastName)
         {
-            FullName = fullName;
-            Age = age;
-            Disease = disease;
+            Name = name;
+            LastName = lastName;
         }
     }
 }
